433e7acb07312ae2d7b87d982890e2355e0a11d7
SALMAN <70966096+salmanabdellatif@users.noreply.github.com>
2024-07-09 15:33:31 +0300

    [1701] [Time Beats: 73.91%] [Memory Beats: 65.22%] - LeetPush

--- 1701-Average-Waiting-Time.js
+++ 1701-Average-Waiting-Time.js
@@ -3,16 +3,15 @@
  * @return {number}
  */
 var averageWaitingTime = function(customers) {
-    let currentTime = 0
     let totalWaitTime = 0
-     for(let i = 0; i < customers.length; i++){
-        let arrivalTime = customers[i][0];
-        let serviceTime = customers[i][1];
+    let currentTime = 0
+    for(let i = 0; i < customers.length; i++){
+        let arrivalTime = customers[i][0]
+        let serviceDuration = customers[i][1]
 
         currentTime = Math.max(currentTime, arrivalTime)
-        let waitTime = currentTime - arrivalTime + serviceTime;
-        totalWaitTime += waitTime;
-        currentTime += serviceTime;
-     }
+        totalWaitTime += currentTime - arrivalTime + serviceDuration
+        currentTime += serviceDuration
+    }
     return totalWaitTime/customers.length
 };
