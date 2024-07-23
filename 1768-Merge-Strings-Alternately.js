b57958f0eb9cbac9b6905080c3cbac95ec826d31
SALMAN <70966096+salmanabdellatif@users.noreply.github.com>
2024-07-23 16:49:31 +0300

    [1768] [Time Beats: 88.61%] [Memory Beats: 85.44%] - LeetPush

--- 1768-Merge-Strings-Alternately.js
+++ 1768-Merge-Strings-Alternately.js
@@ -4,16 +4,41 @@
  * @return {string}
  */
 var mergeAlternately = function(word1, word2) {
-    let i = 0
     let str = ''
-    while(i < word1.length||i < word2.length){
-        if(i < word1.length){
+    let i = 0
+
+    if(word1.length === word2.length){
+        while(i < word1.length){
             str += word1[i]
-        }
-        if(i < word2.length){
             str += word2[i]
+            i++
         }
-        i++
+    }else if(word1.length < word2.length){
+        i = 0
+        while(i < word1.length){
+            str += word1[i]
+            str += word2[i]
+            i++
+        }
+        str += word2.slice(i)
+    }else{
+        i = 0
+        while(i < word2.length){
+            str += word1[i]
+            str += word2[i]
+            i++
+        }
+        str += word1.slice(i)
     }
+
+    // while(i < word1.length||i < word2.length){
+    //     if(i < word1.length){
+    //         str += word1[i]
+    //     }
+    //     if(i < word2.length){
+    //         str += word2[i]
+    //     }
+    //     i++
+    // }
     return str
 };
